47ee69fa995e4316a0b7ca6274de861540b67d5d
Maxim <maksim.krugley@bk.ru>
2023-06-07 01:36:59 +0300

    update circuit, design panel kb, make jlcpcb orders

---
+++ GUI_WPF_Demo/GUI_WPF_Demo/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Runtime.CompilerServices;
+
+namespace GUI_WPF_Demo;
+
+public partial class App : Application
+{
+    public static IHost? AppHost { get; private set; }
+
+    public App()
+    {
+        AppHost = Host.CreateDefaultBuilder()
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddSingleton<MainWindow>();
+                services.AddSingleton<FirstPage>();
+                services.AddSingleton<SecondPage>();
+                services.AddTransient<IWindowContentService, WindowContentService>();
+            })
+            .Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+        await AppHost!.StartAsync();
+
+        MainWindow StartUpWindow = AppHost!.Services.GetRequiredService<MainWindow>()!;
+        StartUpWindow.Content = AppHost!.Services.GetRequiredService<FirstPage>()!;
+        StartUpWindow.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await AppHost!.StopAsync();
+        base.OnExit(e);
+    }
+}
+
+public interface IWindowContentService
+{
+    public void Set<T>() where T : notnull;
+}
+
+public class WindowContentService : IWindowContentService
+{
+    void IWindowContentService.Set<T>() => App.AppHost!.Services.GetRequiredService<MainWindow>()!.Content = App.AppHost!.Services.GetRequiredService<T>()!;
+}
---
+++ GUI_WPF_Demo/GUI_WPF_Demo/SecondPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI_WPF_Demo
+{
+    /// <summary>
+    /// Логика взаимодействия для SecondPage.xaml
+    /// </summary>
+    public partial class SecondPage : UserControl
+    {
+        IWindowContentService _windowContentService;
+
+        public SecondPage(IWindowContentService windowContentService)
+        {
+            _windowContentService = windowContentService;
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _windowContentService.Set<FirstPage>();
+        }
+    }
+}
